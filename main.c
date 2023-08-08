447af5f0385f4c793ecb06d856335b10bcff71d8
Maciej Palczynski <maciej.palczynski@gmail.com>
2023-08-08 18:53:07 +0200

    Add Logger thread

--- main.c
+++ main.c
@@ -13,13 +13,21 @@
 // Status signals of threads
 bool thr_active[3];
 
+
 // Contol main loop
 bool run = true;
 
+// Contol watchdog status
+bool watchdog_active = false;
+
 char *path = "/proc/stat";
+
+// Define Queue for messages
+queue log_messages;
 
 pthread_mutex_t readings_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t thr_acive_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 
 /* Watchdog thread function*/
 
@@ -27,7 +35,7 @@ void *watchdog_thread(void *(vptr_value)) {
 
     while (1) {
         pthread_mutex_lock(&thr_acive_mutex);
-
+        
         // Reset signals from threads
         memset(thr_active, 0, sizeof(thr_active));
     
@@ -38,20 +46,35 @@ void *watchdog_thread(void *(vptr_value)) {
 
         pthread_mutex_lock(&thr_acive_mutex);
 
-        // If any thread signal is not set, then terminate the program due to timeout.
+        // If any thread signal is not set, then stop the program due to timeout.
         if (thr_active[0] != 1) {
+            pthread_mutex_unlock(&thr_acive_mutex);
             printf("Reader timedout!\n");
-            exit(1);
+            message w0 = "Thread 'Reader' timeout!";
+            enqueue(&log_messages, &w0);
+            watchdog_active = true;
+            run = false;
+            
         }
 
         if (thr_active[1] != 1) {
+            pthread_mutex_unlock(&thr_acive_mutex);
             printf("Analyzer timedout!\n");
-            exit(1);
+            message w1 = "Thread 'Analyzer' timeout!";
+            enqueue(&log_messages, &w1);
+            watchdog_active = true;
+            run = false;
+            
         }
 
         if (thr_active[2] != 1) {
+            pthread_mutex_unlock(&thr_acive_mutex);
             printf("Printer timedout!\n");
-            exit(1);
+            message w2 = "Thread 'Printer' timeout!";
+            enqueue(&log_messages, &w2);
+            watchdog_active = true;
+            run = false;
+            
         }
         pthread_mutex_unlock(&thr_acive_mutex);
 
@@ -63,10 +86,20 @@ void *watchdog_thread(void *(vptr_value)) {
 /* Reader thread function */ 
 void *reader_thread(void *vptr_value) {
     
+    char* bar = "Reader thread active!";
+    message* foo;
+    foo = (message*)bar;
     
+    enqueue(&log_messages, foo);
+    
+
+    // Set thread active signal
     pthread_mutex_lock(&thr_acive_mutex);
+
     thr_active[0] = 1;
+
     pthread_mutex_unlock(&thr_acive_mutex);
+
     pthread_mutex_lock(&readings_mutex);
 
     Reading *readings = (Reading *)vptr_value;
@@ -94,15 +127,15 @@ void *reader_thread(void *vptr_value) {
 /* Analyzer thread function */ 
 void *analyzer_thread(void *vptr_value) {
     
+    // Set thread active signal
     pthread_mutex_lock(&thr_acive_mutex);
     thr_active[1] = 1;
     pthread_mutex_unlock(&thr_acive_mutex);
 
     pthread_mutex_lock(&readings_mutex);
-   
-    
+
     Reading *readings = (Reading *)vptr_value;
-    
+
     for (int i=0; i < get_nprocs(); i++) {
             readings[i].cpu_load = calculate_load(&readings[i].st0, &readings[i].st1);
         }
@@ -115,9 +148,18 @@ void *analyzer_thread(void *vptr_value) {
 
 /* Printer thread function */
 void *printer_thread(void *vptr_value) {
+    
+    char* bar = "Printer thread active!";
+    message* foo;
+    foo = (message*)bar;
 
+    enqueue(&log_messages, foo);
+    
+    // Set thread active signal
     pthread_mutex_lock(&thr_acive_mutex);
+
     thr_active[2] = 1;
+
     pthread_mutex_unlock(&thr_acive_mutex);
 
     pthread_mutex_lock(&readings_mutex);
@@ -134,6 +176,22 @@ void *printer_thread(void *vptr_value) {
     return 0;
 }
 
+/* Logger thread function*/
+
+void *logger_thread(void *q_data) {
+    
+    queue *q = (queue *)q_data;
+    if (q->size > 0) {
+        message *m = dequeue(q);
+        FILE *file = fopen("log.txt", "a");
+        fprintf(file, "Front: %i Size: %i %s\n", q->front, q->size, *m);
+        fclose(file);
+
+    }
+    
+    return 0;
+}
+
 /* Signal handler SIGINT = break */
 void sig_handler(int sig_nr) {
     
@@ -142,46 +200,62 @@ void sig_handler(int sig_nr) {
 }
 
 int main (void) {
-    pthread_t reader, analyzer, printer;
+    pthread_t reader, analyzer, printer, watchdog, logger;
 
     int nprocs = get_nprocs();
 
     // Allocate memory according to the processors available in the system.    
     Reading* readings = malloc(nprocs*sizeof(Reading));
-    
+
     // Check if memory allocation is successful
     if (readings == NULL) {
-
-        printf("Unable to allocate memory!");
+        message m0 = "Unable to allocate memory!";
+        enqueue(&log_messages, &m0);
+        logger_thread(&log_messages);
+        printf("Unable to allocate memory!\n");
         return 1;
 
     }
 
+    // Initialise messages queue
+    log_messages.front = 0;
+    log_messages.size = 0;
+   
     printf("This system has %i procesor(s).\n", nprocs);
     printf("\n");
 
     signal(SIGINT, sig_handler);
-    pthread_create(&printer, NULL, watchdog_thread, NULL);
-    
+    pthread_create(&watchdog, NULL, watchdog_thread, NULL);
+   
 
-    while(run) {
-        
+    while(run) { 
+        pthread_create(&logger, NULL, logger_thread, (void *)&log_messages);
         pthread_create(&reader, NULL, reader_thread, (void *)readings);
         pthread_create(&analyzer, NULL, analyzer_thread, (void *)readings);
         pthread_create(&printer, NULL, printer_thread, (void *)readings);
-        // Watchdog
         
         pthread_join(reader, NULL);
         pthread_join(analyzer, NULL);
-        pthread_join(printer, NULL);
-
+        pthread_join(printer, NULL);   
     }
-    
+
     // Free memory and print "good bye" message 
     free(readings);
     
     printf("\n");
-    printf("Program stopped!\n");
 
+    if (!watchdog_active) {
+        printf("Program stopped!\n");
+    }
+    else {
+
+        // Save all queued messages to file
+        while (log_messages.size > 0) {
+        logger_thread(&log_messages);
+
+    }
+        printf("Watchdog stopped the program, please check the log file!\n");
+    }
+    
     return 0;
 }
