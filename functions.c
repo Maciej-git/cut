0a0abf769189ae0f5a53b7dd1113e85d635fe3b5
Maciej Palczynski <maciej.palczynski@gmail.com>
2023-08-09 11:44:01 +0200

    Add functions from main.c to improve project readability

--- functions.c
+++ functions.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdatomic.h>
+#include <stdbool.h>
+#include <sys/sysinfo.h>
+#include <string.h>
+#include <unistd.h>
 
 #define capacity 10
 
@@ -33,6 +37,13 @@ typedef struct _queue {
     atomic_int size;
 } queue;
 
+pthread_mutex_t readings_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t thr_acive_mutex = PTHREAD_MUTEX_INITIALIZER;
+bool thr_active[3];
+bool run = true;
+bool watchdog_active = false;
+queue log_messages;
+char *path = "/proc/stat";
 
 // Add message to queue 
 void enqueue(queue* q, message* data) {
@@ -96,3 +107,167 @@ double calculate_load(struct cpustat *prev, struct cpustat *cur) {
     return cpu_perc;
 }
 
+// Watchdog thread function
+// Tests thr_active signals
+void *watchdog_thread(void *(vptr_value)) {
+
+    while (1) {
+        pthread_mutex_lock(&thr_acive_mutex);
+        
+        // Reset signals from threads
+        memset(thr_active, 0, sizeof(thr_active));
+    
+        pthread_mutex_unlock(&thr_acive_mutex);
+
+        // Wait 2 sec and check signals
+        sleep(2);
+
+        pthread_mutex_lock(&thr_acive_mutex);
+
+        // If any thread signal is not set, then stop the program due to timeout.
+        if (thr_active[0] != 1) {
+            pthread_mutex_unlock(&thr_acive_mutex);
+            printf("Reader timedout!\n");
+            message w0 = "Thread 'Reader' timeout!";
+            enqueue(&log_messages, &w0);
+            watchdog_active = true;
+            run = false;    
+        }
+
+        if (thr_active[1] != 1) {
+            pthread_mutex_unlock(&thr_acive_mutex);
+            printf("Analyzer timedout!\n");
+            message w1 = "Thread 'Analyzer' timeout!";
+            enqueue(&log_messages, &w1);
+            watchdog_active = true;
+            run = false;    
+        }
+
+        if (thr_active[2] != 1) {
+            pthread_mutex_unlock(&thr_acive_mutex);
+            printf("Printer timedout!\n");
+            message w2 = "Thread 'Printer' timeout!";
+            enqueue(&log_messages, &w2);
+            watchdog_active = true;
+            run = false;    
+        }
+        pthread_mutex_unlock(&thr_acive_mutex);
+    }
+    
+    return 0;
+}
+
+// Reader thread function
+void *reader_thread(void *vptr_value) {
+    
+    char* bar = "Reader thread active!";
+    message* foo;
+    foo = (message*)bar;
+    
+    enqueue(&log_messages, foo);
+    
+
+    // Set thread active signal
+    pthread_mutex_lock(&thr_acive_mutex);
+
+    thr_active[0] = 1;
+
+    pthread_mutex_unlock(&thr_acive_mutex);
+
+    pthread_mutex_lock(&readings_mutex);
+
+    Reading *readings = (Reading *)vptr_value;
+    
+    
+    // Perform the first CPU data read and the second after a 1 sec delay.
+    for (int i=0; i < get_nprocs(); i++) {
+        get_stats(&readings[i].st0, i, path);
+        
+    }
+    
+    sleep(1);
+
+    for (int i=0; i < get_nprocs(); i++) {
+
+        get_stats(&readings[i].st1, i, path);
+        
+    }
+
+   pthread_mutex_unlock(&readings_mutex);
+
+   return 0;
+}
+
+// Analyzer thread function
+void *analyzer_thread(void *vptr_value) {
+    
+    // Set thread active signal
+    pthread_mutex_lock(&thr_acive_mutex);
+    thr_active[1] = 1;
+    pthread_mutex_unlock(&thr_acive_mutex);
+
+    pthread_mutex_lock(&readings_mutex);
+
+    Reading *readings = (Reading *)vptr_value;
+
+    for (int i=0; i < get_nprocs(); i++) {
+            readings[i].cpu_load = calculate_load(&readings[i].st0, &readings[i].st1);
+        }
+
+    pthread_mutex_unlock(&readings_mutex);
+    
+    return 0;
+
+}
+
+// Printer thread function
+void *printer_thread(void *vptr_value) {
+    
+    char* bar = "Printer thread active!";
+    message* foo;
+    foo = (message*)bar;
+
+    enqueue(&log_messages, foo);
+    
+    // Set thread active signal
+    pthread_mutex_lock(&thr_acive_mutex);
+
+    thr_active[2] = 1;
+
+    pthread_mutex_unlock(&thr_acive_mutex);
+
+    pthread_mutex_lock(&readings_mutex);
+
+    Reading *readings = (Reading *)vptr_value;
+
+    printf("\n");
+    for (int i=0; i < get_nprocs(); i++) {
+        printf("CPU%i %.2lf %%\n", i, readings[i].cpu_load);
+    }
+
+    pthread_mutex_unlock(&readings_mutex);
+
+    return 0;
+}
+
+// Logger thread function
+void *logger_thread(void *q_data) {
+    
+    queue *q = (queue *)q_data;
+    if (q->size > 0) {
+        message *m = dequeue(q);
+        FILE *file = fopen("log.txt", "a");
+        fprintf(file, "Front: %i Size: %i %s\n", q->front, q->size, *m);
+        fclose(file);
+
+    }
+    
+    return 0;
+}
+
+// Signal handler function
+void sig_handler(int sig_nr) {
+    
+    // Break the main loop on interrupt: User break CTRL+C
+    run = false;
+}
